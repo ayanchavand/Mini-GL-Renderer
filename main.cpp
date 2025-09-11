a0f0d2cdb6e0e2b418a14e239a6953eb11660cfe
ayan <ayan.chavand15@gmail.com>
2025-09-11 23:10:33 +0530

    Window init

--- main.cpp
+++ main.cpp
@@ -3,5 +3,35 @@
 #include <GLFW/glfw3.h>
 
 int main() {
+
+	// Initialize GLFW
+	glfwInit();
+	// Set GLFW to use OpenGL version 3.3 and the core profile
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Create a windowed mode window and its OpenGL context
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Mini OGL renderer", NULL, NULL);
+	if(window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Load OpenGL function pointers using GLAD
+	gladLoadGL();
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(window);
+
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
