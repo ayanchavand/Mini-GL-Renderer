b051315a4fbf90eabc4e2d1b7bcbd3f52d53f888
ayan <ayan.chavand15@gmail.com>
2025-09-17 14:14:26 +0530

    Render method abstraction

--- Renderer.cpp
+++ Renderer.cpp
@@ -16,15 +16,34 @@ void Renderer::BeginFrame(const glm::vec3& bg) {
 	glClearColor(bg.r, bg.g, bg.b, 1.0f); // alpha = 1.0
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	if (wireframeMode) {
-		glDisable(GL_CULL_FACE);
 		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	}
 	else {
-		glEnable(GL_CULL_FACE);
-		glCullFace(GL_BACK);
-		glFrontFace(GL_CCW);
+
 		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 	}
+}
+
+void Renderer::Render(VAO& vao, Shader& shader, const glm::mat4& model, Camera& camera) {
+	shader.Activate();
+
+	// Get vertex shader uniform locations in the GPU
+	GLuint modelLoc = glGetUniformLocation(shader.ID, "model");
+	GLuint viewLoc = glGetUniformLocation(shader.ID, "view");
+	GLuint projLoc = glGetUniformLocation(shader.ID, "projection");
+
+	//upload the model data to the modelLoc 1 4x4 matrix and don't transpose the matrix
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+	
+	glm::mat4 view = camera.GetViewMatrix();
+	glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+	glm::mat4 projection = camera.GetProjectionMatrix();
+	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+	vao.Bind();
+	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+	vao.Unbind();
 }
 
 void Renderer::EndFrame(GLFWwindow* window) {
--- Renderer.h
+++ Renderer.h
@@ -2,14 +2,17 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
-
+#include "Camera.h"
+#include "VAO.h"
+#include "shader.h"
+#include "glm/gtc/type_ptr.hpp"
 class Renderer {
 public:
     Renderer(int width, int height);
 
     void Init();                         // Initialize OpenGL state
     void BeginFrame(const glm::vec3& bg); // Clear screen with background color
-    //void Render(const Mesh& mesh, const Material& mat, const glm::mat4& model, const Camera& camera); // Draw a mesh
+    void Render(VAO& vao, Shader& shader, const glm::mat4& model, Camera& camera); // Draw a mesh
     void EndFrame(GLFWwindow* window);   // Swap buffers and poll events
     //void SetWireframe(bool enable);
 
